f87447ed577799e9e3aef71f9e07bffea7683397
Steve Marshall <steve@stevemarshall.io>
2015-06-15 15:16:20 +0100

    Added user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
-  		# Log the user in
+  		log_in user
+      redirect_to user
   	else
-  		flash[:danger] = 'Invalid email/password combo'
+  		flash.now[:danger] = 'Invalid email/password combo'
   		render 'new'
   	end
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+	# Logs in a given user
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	# returns a logged in user, if any
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	# checks to see if a user is logged in
+	def logged_in?
+		!current_user.nil?
+	end
 end
